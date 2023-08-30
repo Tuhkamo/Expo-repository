a52e19d1a4fa71a536ccd3eb7682d79d7e6d71f4
Tuhkamo <pessa.aapo@gmail.com>
2023-08-30 17:53:11 +0300

    Guessing game complete, with added functionality

--- App.js
+++ App.js
@@ -1,40 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
+import React, { useState, useMemo } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
 
 export default function App() {
-  const [text1, setText1] = useState('');
-  const [text2, setText2] = useState('');
-  const [result, setResult] = useState('');
+  const [guessNumber, setGuessNumber] = useState('');
+  const [numberOfGuesses, setNumberOfGuesses] = useState(1);
+  const [guessText, setGuessText] = useState('Guess a number between 1-100');
+  const [randomNumber, setRandomNumber] = useState(generateRandomNumber());
 
-  const add = () => {
-    setResult('Result: ' + (parseFloat(text1) + parseFloat(text2)));
-  };
+  function generateRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+  }
 
-  const subtract = () => {
-    setResult('Result: ' + (parseFloat(text1) - parseFloat(text2)));
-  };
+  function reset() {
+    setRandomNumber(generateRandomNumber());
+    setNumberOfGuesses(1);
+    setGuessNumber('');
+    setGuessText('Guess a number between 1-100');
+  }
+
+  function buttonPressed() {
+    const guess = parseInt(guessNumber);
+    setNumberOfGuesses((numberOfGuesses + 1));
+
+    if (guess === randomNumber) {
+      Alert.alert(`You guessed the number in ${numberOfGuesses} guesses. Congratulations!`);
+    } else if (guess > randomNumber) {
+      setGuessText(`Your guess ${guess} was too high`);
+    } else if (guess < randomNumber) {
+      setGuessText(`Your guess ${guess} was too low`);
+    }
+    
+  }
 
   return (
     <View style={styles.container}>
-      <Text>{result}</Text>
+      <Text style={styles.guessText}>{guessText}</Text>
       <TextInput
         style={styles.input}
-        onChangeText={(text) => setText1(text)}
-        value={text1}
+        onChangeText={text => setGuessNumber(text)}
+        value={guessNumber}
         keyboardType="numeric"
       />
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => setText2(text)}
-        value={text2}
-        keyboardType="numeric"
-      />
-      <View style={styles.buttonGroup}>
-        <Button onPress={add} title="+" />
-        <View style={styles.space}/>
-        <Button onPress={subtract} title="-" />
-      </View>
+      <Button onPress={buttonPressed} title="Make Guess" />
+      <Button onPress={reset} title="Reset" />
       <StatusBar style="auto" />
     </View>
   );
@@ -45,18 +54,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
   input: {
+    marginTop: 10,
+    marginBottom: 5,
     width: 200,
     borderColor: 'gray',
     borderWidth: 1,
   },
-  buttonGroup: {
-    flexDirection: 'row',
+  guessText: {
+    marginTop: 50,
+    fontSize: 18,
+    textAlign: 'center',
   },
-  space: {
-    width: 20,
-    height: 20,
-  }
 });
