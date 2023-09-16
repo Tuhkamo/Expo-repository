fccf7277dc81ad25a7a4d915fa41959c59c1865d
Tuhkamo <pessa.aapo@gmail.com>
2023-09-16 13:05:38 +0300

    Finished recipe search with names and pictures, used react-native-paper for additional styling

--- App.js
+++ App.js
@@ -1,21 +1,75 @@
-// App.js
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Calculator from './Calculator';
-import History from './History';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, FlatList, StyleSheet } from 'react-native';
+import { Card, Title } from 'react-native-paper';
 
-const Stack = createStackNavigator();
+const API_KEY = '1';
 
-const App = () => {
+export default function App() {
+  const [query, setQuery] = useState('');
+  const [recipes, setRecipes] = useState([]);
+
+  const searchRecipes = async () => {
+    try {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/${API_KEY}/search.php?s=${query}`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setRecipes(data.meals);
+    } catch (error) {
+      console.error('Error fetching recipes:', error);
+    }
+  };
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Calculator">
-        <Stack.Screen name="Calculator" component={Calculator} />
-        <Stack.Screen name="History" component={History} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text style={styles.header}>Recipe Search</Text>
+      <TextInput
+        placeholder="Enter recipe name..."
+        style={styles.input}
+        onChangeText={(text) => setQuery(text)}
+        value={query}
+      />
+      <Button title="Search" onPress={searchRecipes} />
+      <FlatList
+        data={recipes}
+        keyExtractor={(item) => item.idMeal}
+        renderItem={({ item }) => (
+          <Card style={styles.recipeContainer}>
+            <Card.Cover source={{ uri: `${item.strMealThumb}/preview` }} resizeMode="contain" style={styles.image} />
+            <Card.Content>
+              <Title>{item.strMeal}</Title>
+            </Card.Content>
+          </Card>
+        )}
+      />
+    </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    alignItems: 'flex-start',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingLeft: 8,
+  },
+  recipeContainer: {
+    marginVertical: 8,
+  },
+  image: {
+    aspectRatio: 1,
+  },
+});
